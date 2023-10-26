1590e036a86b75c7f1904e85dd57fca03018a19f
OssieTM <tomugocha@gmail.com>
2023-10-26 08:01:33 -0700

    2-strlen_recursion.c rev

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int l = 0;
+	int length  = 0;
+
+	if (*s)
 	{
-		l++;
-		l += _strlen_recursion(s + 1);
+		length++;
+		length += _strlen_recursion(s + 1);
 	}
-	return (l);
+	return (length);
 }
